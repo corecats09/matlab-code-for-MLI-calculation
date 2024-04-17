185aa09ce44ffef619c67d652627aa5f79c18e76
fwafdaijfjds <149335922+corecats09@users.noreply.github.com>
2024-04-17 09:54:47 +0100

    Update MLE_cal.m

--- MLE_cal.m
+++ MLE_cal.m
@@ -28,28 +28,29 @@ residual_sta(:,n) = tbl_ele_adj*param_sta_adj(:,n)-mean_sta;
 std_residual_sta(1,n) = std (residual_sta(:,n));
 end
 for n = 1:15
-lnLmax_sta(:,n) = n.*log(sqrt(2.*pi).*std_residual_sta(1,n))- norm(residual_sta(:,n)).^2./2./(std_residual_sta(1,n).^2);
+lnLmax_sta(:,n) = length(mean_sta).*log(sqrt(2.*pi).*std_residual_sta(1,n))- norm(residual_sta(:,n)-mean(residual_sta(:,n)))^4./2./(std_residual_sta(1,n)^2);
 end
 for n = 1:15
-cov_temp =std_residual_sta(1,n)^2./(length(mean_log)-length(n)) * inv(tbl_ele_adj(:,1:n)'* tbl_ele_adj(:,1:n));
+cov_temp =std_residual_sta(1,n)^2* inv(tbl_ele_adj(:,1:n)'* tbl_ele_adj(:,1:n));
 det_cov_sta(1,n) = det(cov_temp);
 end
 for n = 1:15
-lnMLI_sta(1,n) = n/2.*log(2*pi)+lnLmax_sta(1,n)+0.5.*log(det_cov_sta(1,n))-n.*log(2500);
+lnMLI_sta(1,n) = n/2.*log(2*pi)+lnLmax_sta(1,n)+0.5.*log(det_cov_sta(1,n))-n.*log(1);
 end
+
 for n = 1:15
 residual_log(:,n) = tbl_ele_adj*param_log_adj(:,n)-mean_log;
 std_residual_log(1,n) = std (residual_log(:,n));
 end
 for n = 1:15
-lnLmax_log(:,n) = n.*log(sqrt(2.*pi).*std_residual_log(1,n))-norm(residual_log(:,n)).^2./2./(std_residual_log(1,n).^2);
+lnLmax_log(:,n) = length(mean_log).*log(sqrt(2.*pi).*std_residual_log(1,n))-norm(residual_log(:,n)-mean(residual_log(:,n)))^4./2./(std_residual_log(1,n)^2);
 end
 for n = 1:15
-cov_temp =std_residual_log(1,n)^2./(length(mean_sta)-length(n))  * inv(tbl_ele_adj(:,1:n)'* tbl_ele_adj(:,1:n));
+cov_temp =std_residual_log(1,n)^2  * inv(tbl_ele_adj(:,1:n)'* tbl_ele_adj(:,1:n));
 det_cov_log(1,n) = det(cov_temp);
 end
 for n = 1:15
-lnMLI_log(1,n) = n/2.*log(2*pi)+lnLmax_log(1,n)+0.5.*log(det_cov_log(1,n))-n.*log(1500);
+lnMLI_log(1,n) = n/2.*log(2*pi)+lnLmax_log(1,n)+0.5.*log(det_cov_log(1,n))-n.*log(1);
 end
 
 scatter (linspace(1,15,15),lnMLI_log)
