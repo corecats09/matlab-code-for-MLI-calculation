a23794af0cd19f6dc82b771b171f53e92cd71bca
fwafdaijfjds <149335922+corecats09@users.noreply.github.com>
2024-05-03 10:07:47 +0100

    Update MLE_cal.m

--- MLE_cal.m
+++ MLE_cal.m
@@ -2,23 +2,23 @@ param_log_adj = zeros(15,15);
 for m = 1:15
 cvx_begin
 A = []; 
-variable A(m, 1); 
-minimize( norm(tbl_ele_adj (:,1:m) *A-mean_log, 'fro') );
+variable A(m+1, 1); 
+minimize( norm(tbl_ele_adj (:,1:m+1) *A-mean_log, 'fro') );
 subject to
 A >= 0; % 所有元素非负
 cvx_end; 
-param_log_adj(1:m,m)=A;
+param_log_adj(1:m+1,m)= A;
 end
 param_sta_adj=zeros(15,15);
 for m = 1:15
 cvx_begin
-A = []; 
-variable A(m, 1); 
-minimize( norm(tbl_ele_adj (:,1:m) *A-mean_sta, 'fro') );
+A = [];  
+variable A(m+1, 1); 
+minimize( norm(tbl_ele_adj (:,1:m+1) *A-mean_sta, 'fro') );
 subject to
 A >= 0; % 所有元素非负
-cvx_end;
-param_sta_adj(1:m,m)=A;
+cvx_end; 
+param_sta_adj(1:m+1,m)=A;
 end
 
 
@@ -27,14 +27,14 @@ for n = 1:15
 residual_sta(:,n) = tbl_ele_adj*param_sta_adj(:,n)-mean_sta;
 std_residual_sta(1,n) = std (residual_sta(:,n));
 end
-for n = 1:15
-lnLmax_sta(:,n) = length(mean_sta).*log(sqrt(2.*pi).*std_residual_sta(1,n))- norm(residual_sta(:,n)-mean(residual_sta(:,n)))^4./2./(std_residual_sta(1,n)^2);
+for n =  1:15
+lnLmax_sta(:,n) = -length(mean_sta)./2.*log(sqrt(2.*pi).*std_residual_sta(1,n)^2)- norm(residual_sta(:,n)-mean(residual_sta(:,n)))^2./2./(std_residual_sta(1,n)^2);
 end
-for n = 1:15
-cov_temp =std_residual_sta(1,n)^2* inv(tbl_ele_adj(:,1:n)'* tbl_ele_adj(:,1:n));
+for n =  1:15
+cov_temp =std_residual_sta(1,n)^2* inv(tbl_ele_adj(:,1:n+1)'* tbl_ele_adj(:,1:n+1));
 det_cov_sta(1,n) = det(cov_temp);
 end
-for n = 1:15
+for n =  1:15
 lnMLI_sta(1,n) = n/2.*log(2*pi)+lnLmax_sta(1,n)+0.5.*log(det_cov_sta(1,n))-n.*log(1);
 end
 
@@ -42,14 +42,14 @@ for n = 1:15
 residual_log(:,n) = tbl_ele_adj*param_log_adj(:,n)-mean_log;
 std_residual_log(1,n) = std (residual_log(:,n));
 end
-for n = 1:15
-lnLmax_log(:,n) = length(mean_log).*log(sqrt(2.*pi).*std_residual_log(1,n))-norm(residual_log(:,n)-mean(residual_log(:,n)))^4./2./(std_residual_log(1,n)^2);
+for n =  1:15
+lnLmax_log(:,n) = -length(mean_log)./2.*log(sqrt(2.*pi).*std_residual_log(1,n)^2)- norm(residual_log(:,n)-mean(residual_log(:,n)))^2./2./(std_residual_log(1,n)^2);
 end
-for n = 1:15
-cov_temp =std_residual_log(1,n)^2  * inv(tbl_ele_adj(:,1:n)'* tbl_ele_adj(:,1:n));
+for n =  1:15
+cov_temp =std_residual_sta(1,n)^2* inv(tbl_ele_adj(:,1:n+1)'* tbl_ele_adj(:,1:n+1));
 det_cov_log(1,n) = det(cov_temp);
 end
-for n = 1:15
+for n =  1:15
 lnMLI_log(1,n) = n/2.*log(2*pi)+lnLmax_log(1,n)+0.5.*log(det_cov_log(1,n))-n.*log(1);
 end
 
